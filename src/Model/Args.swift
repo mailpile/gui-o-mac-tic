ef342b5c9e3c535f3bbadd20f4e37cd166540017
Pétur Ingi Egilsson <petur@ingi.dk>
2018-03-24 12:36:38 +0100

    Commenting.

--- src/Model/Args.swift
+++ src/Model/Args.swift
@@ -6,6 +6,8 @@ import Foundation
     let dictionary: [String: String]?
     
     init(string: String?, list: [String]?, dictionary: [String: String]?) {
+        /* TODO error checking: xor shall be true between:
+         string, list and dictionary.*/
         self.string = string
         self.list = list
         self.dictionary = dictionary
