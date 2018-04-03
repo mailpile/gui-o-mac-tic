71ade9359da56d8248de98c80b4ef2e3a43768b0
Pétur Ingi Egilsson <petur@ingi.dk>
2018-04-03 11:45:56 +0200

    Error detection of stage 3.

--- src/NetworkError.swift
+++ src/NetworkError.swift
@@ -1,6 +1,8 @@
 import Foundation
 
 enum NetworkError: Error {
-    case getaddrinfo(reason: String)
-    case socket(reason: String)
+    case getaddrinfo(errorMessage: String)
+    case socket(errorMessage: String)
+    case bind(recoverable: Bool, errorMessage: String)
+    case listen(recoverable: Bool, errorMessage: String)
 }
--- src/Server.swift
+++ src/Server.swift
@@ -1,17 +1,18 @@
 import Foundation
 import CoreFoundation
 
-/* NOTE: The archaic functions used in this file are documented in the online-manual pages (man command). */
-
 class Server: Thread {
     let QUEUE_SIZE: Int32 = 5
     let port = "4444"
     let UNKNOWN_ERROR = "Unknown error."
     
+    /* NOTE: The archaic types, functions and their error-codes
+     * are documented in the BSD System Calls Manual. */
+    
     var _addrinfo: addrinfo?
     var _sockaddr: UnsafeMutablePointer<addrinfo>?
-    var socket_fd: Int32 = -1
-    var request_fd: Int32 = -1
+    var socket_fd: Int32? = -1
+    var request_fd: Int32? = -1
 
     func serve(dispatchForExecutionWhenChannelIsOpened: () -> Void) {
         func setupDataStructures() throws {
@@ -32,11 +33,11 @@ class Server: Thread {
                 &(self._sockaddr))
             
             guard status == 0 else {
-                let prefix = "The following error occured while preparing for a network connection: "
+                let prefix = "Failed to prepare for a network connection: "
                 let errorMessage = status == EAI_SYSTEM
                     ? String(validatingUTF8: strerror(errno))
                     : String(validatingUTF8: gai_strerror(status))
-                throw NetworkError.getaddrinfo(reason: prefix + (errorMessage ?? UNKNOWN_ERROR))
+                throw NetworkError.getaddrinfo(errorMessage: prefix + (errorMessage ?? UNKNOWN_ERROR))
             }
         }
         
@@ -47,43 +48,89 @@ class Server: Thread {
                 self._sockaddr!.pointee.ai_protocol)
             
             guard self.socket_fd != -1 else {
-                var errorMessage = "The following error occured while attempting to create a socket file descriptor: "
+                var errorMessage = "Failed to create a socket file descriptor. Error: "
                 errorMessage.append(String(utf8String: strerror(errno)) ?? UNKNOWN_ERROR)
                 freeaddrinfo(self._sockaddr)
-                throw NetworkError.socket(reason: errorMessage)
+                throw NetworkError.socket(errorMessage: errorMessage)
             }
         }
         
-        func bindSocketFileDescriptorToPort() {
+        func bindSocketFileDescriptorToPort() throws {
             let status = Darwin.bind(
-                self.socket_fd,
+                self.socket_fd!,
                 self._sockaddr!.pointee.ai_addr,
                 self._sockaddr!.pointee.ai_addrlen)
             
             guard status == 0 else {
-                // Handle errors
-                freeaddrinfo(self._sockaddr)
-                close(self.socket_fd)
-                return
+                defer {
+                    freeaddrinfo(self._sockaddr)
+                    close(self.socket_fd!)
+                }
+                
+                var errorMessage = "Failed to bind a socket file descriptor to a port. Error: "
+                errorMessage.append(String(utf8String: strerror(errno)) ?? UNKNOWN_ERROR)
+                
+                switch errno {
+                                    /** Recoverable errors: **/
+                case EADDRINUSE,    /* The specified address is already in use. */
+                     EINVAL:        /* socket is already bound or it has been shut down. */
+                    throw NetworkError.bind(recoverable: true, errorMessage: errorMessage)
+                    
+                                    /** Non-recoverable errors: **/
+                case EACCES,        /* The current user has inadequate permission to access the requested address. */
+                     EADDRNOTAVAIL, /* The specified address is not available from the local machine. */
+                     EAFNOSUPPORT,  /* socket is not of a type that can be bound to an address. */
+                     EBADF,         /* socket is not a valid file descriptor. */
+                     EDESTADDRREQ,  /* socket is a null pointer. */
+                     EFAULT,        /* the address parameter is not in a valid part of the user address space. */
+                     ENOTSOCK:      /* socket does not refer to a socket */
+                    break
+                default:            /* Non-documented errors are non-recoverable. */
+                    break
+                }
+                throw NetworkError.bind(recoverable: false, errorMessage: errorMessage)
             }
+            freeaddrinfo(self._sockaddr)
         }
         
-        func listenForACall() {
+        func listenForACall() throws {
             let status = listen(
-                self.socket_fd,
+                self.socket_fd!,
                 QUEUE_SIZE)
             
             guard status == 0 else {
-                // TODO error handling.
-                close(self.socket_fd)
-                return
+                defer {
+                    close(socket_fd!)
+                }
+                
+                var errorMessage = "Failed to listen on a socket. Error: "
+                errorMessage.append(String(utf8String: strerror(errno)) ?? UNKNOWN_ERROR)
+                
+                switch errno {
+                                    /** Recoverable errors: **/
+                case EDESTADDRREQ,  /* The socket is not bound to a local address and the protocol
+                                     * does not support listening to an unbound socket. */
+                     EINVAL:        /* Socket is already connected. */
+                    throw NetworkError.listen(recoverable: true, errorMessage: errorMessage)
+                    
+                                    /** Non-recoverable errors: **/
+                case EACCES,        /* The current process has insufficient privileges. */
+                     EBADF,         /* The argument socket is not a valid file descriptor. */
+                     EINVAL,        /* Socket is already connected. */
+                     ENOTSOCK,      /* The argument socket does not reference a socket. */
+                     EOPNOTSUPP:    /* The socket is not of a type that supports the operation listen(). */
+                    break
+                default:            /* Non-documented errors are non-recoverable. */
+                    break
+                }
+                throw NetworkError.listen(recoverable: false, errorMessage: errorMessage)
             }
         }
         
         func acceptCall() {
             var connectedAddrInfo = sockaddr(sa_len: 0, sa_family: 0, sa_data: (0,0,0,0,0,0,0,0,0,0,0,0,0,0))
             var connectedAddrInfoLength = socklen_t(MemoryLayout.size(ofValue: sockaddr.self))
-            self.request_fd = accept(self.socket_fd,
+            self.request_fd = accept(self.socket_fd!,
                                      &connectedAddrInfo,
                                      &connectedAddrInfoLength) // TODO free?
             
@@ -100,7 +147,7 @@ class Server: Thread {
                 var line: String = ""
                 var buff_rcvd = CChar()
                 while line.last != "\n" {
-                    read(self.request_fd,
+                    read(self.request_fd!,
                          &buff_rcvd,
                          1)
                     line.append(NSString(format:"%c",buff_rcvd) as String)
@@ -117,8 +164,8 @@ class Server: Thread {
         do {
             try setupDataStructures()
             try getSocketFileDescriptor()
-            bindSocketFileDescriptorToPort()
-            listenForACall()
+            try bindSocketFileDescriptorToPort()
+            try listenForACall()
             dispatchForExecutionWhenChannelIsOpened()
             acceptCall()
             receiveAndProcessData()
